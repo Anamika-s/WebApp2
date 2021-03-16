cd5fd6d1e1918bc3567d439fb037868d30c87c8b
Anamika <anamika.sawhney22@gmail.com>
2021-03-16 13:37:10 +0530

    Add project files.

---
+++ WebApplication3/Migrations/20210316080109_name.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication3.Migrations
+{
+    public partial class name : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Batch = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Marks = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Batch", "Marks", "Name" },
+                values: new object[] { 1, "B001", 90, "Ajay" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Batch", "Marks", "Name" },
+                values: new object[] { 2, "B002", 98, "Deepak" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ WebApplication3/Models/Student.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Batch { get; set; }
+        public int Marks { get; set; }
+    }
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options)
+            : base(options) { }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().
+                HasData(new Student
+                {
+                    Id = 1,
+                    Name = "Ajay",
+                    Batch = "B001",
+                    Marks = 90
+                }, new Student
+                {
+                    Id = 2,
+                    Name = "Deepak",
+                    Batch = "B002",
+                    Marks = 98
+                }
+                );
+
+
+        }
+
+    }
+}
