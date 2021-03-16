2447a26175892a71b67fc0b2d93af7cd7e4aeda7
Anamika <anamika.sawhney22@gmail.com>
2021-03-16 13:55:57 +0530

    aaa

--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -43,7 +43,7 @@ namespace WebApplication3
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
